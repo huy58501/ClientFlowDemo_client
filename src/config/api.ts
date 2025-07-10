9f0cbc5ff5eaaf3ca376b4f2e3d58a76b1b39cfd
HOANG CHUONG NGUYEN <97963939+huy58501@users.noreply.github.com>
2025-07-10 08:12:28 -0400

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = process.env.API_BASE_URL;
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = process.env.API_BASE_URL;
+//const API_BASE_URL = 'http://localhost:8080';
 
 export const API_ROUTES = {
   LOGIN: `${API_BASE_URL}/auth/login`,
